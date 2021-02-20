226b7320d20eeadca9b4beebd2eae18e92577ce7
yang985 <z1158613396@gmail.com>
2021-02-20 08:24:29 +0000

    center

--- src/pages/Index/center/center.jsx
+++ src/pages/Index/center/center.jsx
@@ -6,16 +6,18 @@ import styles from './center.less';
 const mockData = [
   {
     images: 'https://webdesignfacts.net/wp-content/uploads/2019/01/CSS-Background-Fireflies.gif',
-    span1: '测试1',
-    span2: '我觉得可以',
-    buttonTitle: '点击进入',
+    span1: 'Artificial',
+    span2: 'Intelligence for',
+    span3: 'Everyone',
+    buttonTitle: 'Learn More',
     buttonLink: 'https://www.baidu.com/',
   },
   {
     images: 'https://webdesignfacts.net/wp-content/uploads/2019/01/CSS-Background-Fireflies.gif',
-    span1: '测试2',
-    span2: '我觉得可以',
-    buttonTitle: '点击进入',
+    span1: 'Artificial',
+    span2: 'Intelligence for',
+    span3: 'Everyone',
+    buttonTitle: 'Learn More',
     buttonLink: 'https://www.baidu.com/',
   },
 ];
@@ -36,13 +38,20 @@ class center extends Component {
                   className={styles.contentstyle}
                   style={{ backgroundImage: `url(${item.images})` }}
                 >
+                  <br />
                   <span>
                     {item.span1}
                     <br />
                     {item.span2}
+                    <br />
+                    {item.span3}
                   </span>
                   <br />
-                  <Button onClick={this.homeOpenTab.bind(this, item.buttonLink)} size="large">
+                  <Button
+                    className={styles.buttonstyle}
+                    onClick={this.homeOpenTab.bind(this, item.buttonLink)}
+                    size="large"
+                  >
                     {item.buttonTitle}
                   </Button>
                 </div>
