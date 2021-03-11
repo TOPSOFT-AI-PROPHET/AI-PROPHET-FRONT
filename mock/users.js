542cf4bff198843a4b5a2395c2fc8258074066cd
yang985 <z1158613396@gmail.com>
2021-03-11 20:58:29 +0000

    workplace page

--- mock/users.js
+++ mock/users.js
@@ -35,8 +35,65 @@ export default {
         data: {
           username: 'f9960ff9-6a85-4458-822b-f777022d8eeb',
           email: 'f9960ff9-6a85-4458-822b-f777022d8eeb@qq.com',
-          nickname: '',
+          nickname: 'zhang yang',
+          balance: '666',
+          profile_url: 'http://m.imeitou.com/uploads/allimg/2020031310/tt42dal51ms.jpeg',
+          total_tasks: '',
         },
+      });
+      return;
+    }
+
+    res.status(401).send({
+      detail: 'Given token not valid for any token type',
+      code: 'token_not_valid',
+      messages: [
+        {
+          token_class: 'AccessToken',
+          token_type: 'access',
+          message: 'Token is invalid or expired',
+        },
+      ],
+    });
+  },
+
+  'POST /tasks/list': async (req, res) => {
+    if (req.headers.authorization === 'Bearer valid_token') {
+      res.send({
+        code: 200,
+        message: 'Bingo',
+        data: [
+          {
+            model: 'task.task',
+            pk: 1,
+            fields: {
+              user_id: 4,
+              ai_id: 1,
+              ai_json: 'asd',
+              ai_result: 'asd',
+              description: 'asd',
+              status: 0,
+              time_start: '2021-03-10T08:50:36Z',
+              time_done: null,
+              is_delete: 0,
+            },
+          },
+          {
+            model: 'task.task',
+            pk: 2,
+            fields: {
+              user_id: 4,
+              ai_id: 1,
+              ai_json: 'asd',
+              ai_result: 'asd',
+              description: 'asd',
+              status: 0,
+              time_start: '2021-03-10T08:50:36Z',
+              time_done: null,
+              is_delete: 0,
+            },
+          },
+        ],
       });
       return;
     }
--- src/pages/dashboard/workplace/index.jsx
+++ src/pages/dashboard/workplace/index.jsx
@@ -1,38 +1,11 @@
-import { Avatar, Card, Col, List, Skeleton, Row, Statistic } from 'antd';
+import { Avatar, Card, List, Skeleton, Statistic } from 'antd';
 import React, { Component } from 'react';
-import { Link, connect } from 'umi';
+import { connect, history } from 'umi';
 import { PageContainer } from '@ant-design/pro-layout';
 import moment from 'moment';
-import Radar from './components/Radar';
-import EditableLinkGroup from './components/EditableLinkGroup';
+import { FormattedMessage } from 'umi';
 import styles from './style.less';
-
-const links = [
-  {
-    title: '操作一',
-    href: '',
-  },
-  {
-    title: '操作二',
-    href: '',
-  },
-  {
-    title: '操作三',
-    href: '',
-  },
-  {
-    title: '操作四',
-    href: '',
-  },
-  {
-    title: '操作五',
-    href: '',
-  },
-  {
-    title: '操作六',
-    href: '',
-  },
-];
+import request from '@/utils/request';
 
 const PageHeaderContent = ({ currentUser }) => {
   const loading = currentUser && Object.keys(currentUser).length;
@@ -52,42 +25,65 @@ const PageHeaderContent = ({ currentUser }) => {
   return (
     <div className={styles.pageHeaderContent}>
       <div className={styles.avatar}>
-        <Avatar
-          size="large"
-          src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png"
-        />
+        <Avatar size="large" src={currentUser.profile_url} />
       </div>
       <div className={styles.content}>
         <div className={styles.contentTitle}>
-          早安，
+          Hello，
           {currentUser.nickname !== '' ? currentUser.nickname : '路人甲'}
-          ，祝你开心每一天！
         </div>
-        <div>仰天大笑出门去，我辈岂是蓬蒿人。</div>
+        <div>你看你妈呢</div>
       </div>
     </div>
   );
 };
 
-const ExtraContent = () => (
+const ExtraContent = ({ currentUser }) => (
   <div className={styles.extraContent}>
     <div className={styles.statItem}>
-      <Statistic title="项目数" value={56} />
+      <Statistic
+        title={
+          <FormattedMessage
+            id="pages.dashboardandworkplace.workplace.numtask"
+            defaultMessage="已完成预测"
+          />
+        }
+        value={currentUser.total_tasks}
+      />
     </div>
     <div className={styles.statItem}>
-      <Statistic title="团队内排名" value={8} suffix="/ 24" />
-    </div>
-    <div className={styles.statItem}>
-      <Statistic title="项目访问" value={2223} />
+      <Statistic
+        title={
+          <FormattedMessage
+            id="pages.dashboardandworkplace.workplace.balance"
+            defaultMessage="余额"
+          />
+        }
+        value={currentUser.balance}
+      />
     </div>
   </div>
 );
 
 class Workplace extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      newdata: [],
+    };
+  }
+
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch({
       type: 'dashboardAndworkplace/init',
+    });
+
+    request('/tasks/list', { method: 'POST' }).then((result) => {
+      this.setState({
+        data: result.data.slice(0, 9),
+      });
     });
   }
 
@@ -132,14 +128,7 @@ class Workplace extends Component {
   };
 
   render() {
-    const {
-      currentUser,
-      activities,
-      projectNotice,
-      projectLoading,
-      activitiesLoading,
-      radarData,
-    } = this.props;
+    const { currentUser, projectLoading } = this.props;
 
     if (!currentUser || !currentUser.username) {
       return null;
@@ -148,119 +137,67 @@ class Workplace extends Component {
     return (
       <PageContainer
         content={<PageHeaderContent currentUser={currentUser} />}
-        extraContent={<ExtraContent />}
+        extraContent={<ExtraContent currentUser={currentUser} />}
       >
-        <Row gutter={24}>
-          <Col xl={16} lg={24} md={24} sm={24} xs={24}>
-            <Card
-              className={styles.projectList}
-              style={{
-                marginBottom: 24,
-              }}
-              title="进行中的项目"
-              bordered={false}
-              extra={<Link to="/">全部项目</Link>}
-              loading={projectLoading}
-              bodyStyle={{
-                padding: 0,
+        <p>{console.log(this.state.data)}</p>
+
+        <Card
+          className={styles.projectList}
+          style={{
+            marginBottom: 24,
+          }}
+          title="My Predictions"
+          bordered={false}
+          extra={
+            <a
+              onClick={() => {
+                history.push('/dash/dashboard/list/basic-list');
               }}
             >
-              {projectNotice.map((item) => (
-                <Card.Grid className={styles.projectGrid} key={item.id}>
-                  <Card
-                    bodyStyle={{
-                      padding: 0,
-                    }}
-                    bordered={false}
-                  >
-                    <Card.Meta
-                      title={
-                        <div className={styles.cardTitle}>
-                          <Avatar size="small" src={item.logo} />
-                          <Link to={item.href}>{item.title}</Link>
-                        </div>
-                      }
-                      description={item.description}
-                    />
-                    <div className={styles.projectItemContent}>
-                      <Link to={item.memberLink}>{item.member || ''}</Link>
-                      {item.updatedAt && (
-                        <span className={styles.datetime} title={item.updatedAt}>
-                          {moment(item.updatedAt).fromNow()}
-                        </span>
-                      )}
+              Total Predictions
+            </a>
+          }
+          loading={projectLoading}
+          bodyStyle={{
+            padding: 0,
+          }}
+        >
+          {this.state.data.map((item) => (
+            <Card.Grid className={styles.projectGrid} key={item.pk}>
+              <Card
+                bodyStyle={{
+                  padding: 0,
+                }}
+                bordered={false}
+              >
+                <Card.Meta
+                  title={
+                    <div className={styles.cardTitle}>
+                      <Avatar
+                        size="small"
+                        src="http://m.imeitou.com/uploads/allimg/2020031310/tt42dal51ms.jpeg"
+                      />
+                      <a>{item.model}</a>
                     </div>
-                  </Card>
-                </Card.Grid>
-              ))}
-            </Card>
-            <Card
-              bodyStyle={{
-                padding: 0,
-              }}
-              bordered={false}
-              className={styles.activeCard}
-              title="动态"
-              loading={activitiesLoading}
-            >
-              <List
-                loading={activitiesLoading}
-                renderItem={(item) => this.renderActivities(item)}
-                dataSource={activities}
-                className={styles.activitiesList}
-                size="large"
-              />
-            </Card>
-          </Col>
-          <Col xl={8} lg={24} md={24} sm={24} xs={24}>
-            <Card
-              style={{
-                marginBottom: 24,
-              }}
-              title="快速开始 / 便捷导航"
-              bordered={false}
-              bodyStyle={{
-                padding: 0,
-              }}
-            >
-              <EditableLinkGroup onAdd={() => {}} links={links} linkElement={Link} />
-            </Card>
-            <Card
-              style={{
-                marginBottom: 24,
-              }}
-              bordered={false}
-              title="XX 指数"
-              loading={radarData.length === 0}
-            >
-              <div className={styles.chart}>
-                <Radar hasLegend height={343} data={radarData} />
-              </div>
-            </Card>
-            <Card
-              bodyStyle={{
-                paddingTop: 12,
-                paddingBottom: 12,
-              }}
-              bordered={false}
-              title="团队"
-              loading={projectLoading}
-            >
-              <div className={styles.members}>
-                <Row gutter={48}>
-                  {projectNotice.map((item) => (
-                    <Col span={12} key={`members-item-${item.id}`}>
-                      <Link to={item.href}>
-                        <Avatar src={item.logo} size="small" />
-                        <span className={styles.member}>{item.member}</span>
-                      </Link>
-                    </Col>
-                  ))}
-                </Row>
-              </div>
-            </Card>
-          </Col>
-        </Row>
+                  }
+                  description={item.fields.description}
+                />
+                {/* <p>{item.fields.description}</p> */}
+                <div style={{ textAlign: 'right', fontSize: 12 }}>
+                  {/* <span  className = {styles.datatime} >
+                    {item.fields.time_start}
+                  </span>
+                  {/*item.updatedAt && (
+                    <span className={styles.datetime} title={item.updatedAt}>
+                      {moment(item.updatedAt).fromNow()}
+                    </span>
+                  )} */}
+                  <p> {item.fields.time_start} </p>
+                </div>
+              </Card>
+            </Card.Grid>
+          ))}
+        </Card>
       </PageContainer>
     );
   }
