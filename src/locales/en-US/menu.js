66b205ba2f69f278d9265f1b18fb3500f0c96b6e
奚玉帆 <568875138@qq.com>
2021-04-06 23:28:46 +0800

    Update menu.js

--- src/locales/en-US/menu.js
+++ src/locales/en-US/menu.js
@@ -8,6 +8,7 @@ export default {
   'menu.register': 'Register',
   'menu.register.result': 'Register Result',
   'menu.dashboard': 'Dashboard',
+  'menu.prediction': 'AI Prediction',
   'menu.dashboard.analysis': 'Analysis',
   'menu.dashboard.monitor': 'Monitor',
   'menu.dashboard.workplace': 'Workplace',
