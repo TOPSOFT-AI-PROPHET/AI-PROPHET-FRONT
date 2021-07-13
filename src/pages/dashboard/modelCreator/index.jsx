d5f94e624566d4e36d8ad6d4a3e9af8209d056f9
yang985 <z1158613396@gmail.com>
2021-07-13 21:44:56 +0800

    取消upload上传

--- src/pages/dashboard/modelCreator/index.jsx
+++ src/pages/dashboard/modelCreator/index.jsx
@@ -119,10 +119,11 @@ export default class ModelCreator extends React.Component {
 
   beforeUpload = (file) => {
     console.log(file);
-    const isLt800M = file.size / 1024 / 1024 < 800; // limited picture size(not using)
+    const isLt800M = file.size / 1024 / 1024 < 2; // limited picture size(not using)
     if (!isLt800M) {
       message.error('文件应当小于800MB');
       // error message for valid size
+      return false;
     }
     const reader = new FileReader();
     reader.readAsBinaryString(file);
@@ -133,7 +134,7 @@ export default class ModelCreator extends React.Component {
         dataSet: e.target.result,
       });
     };
-    return isLt800M;
+    return false;
   };
 
   setcreditModalVisible(creditModalVisible) {
